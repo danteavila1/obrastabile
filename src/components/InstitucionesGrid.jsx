96a3fe7d8868b46fb934c5a5d46f523fc078bb31
Dante Avila <danteavila1@hotmail.com>
2025-09-21 21:10:51 -0300

    logos puestos en el grid

--- src/components/InstitucionesGrid.jsx
+++ src/components/InstitucionesGrid.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import logo1 from "../assets/logos/observatorio.webp"; // <-- import
+import logo1 from "../assets/logos/observatorio.webp";
+import logo2 from "../assets/logos/clubstabile.webp";
+import logo3 from "../assets/logos/congreso.webp";
+import logo4 from "../assets/logos/buenaspracticas.webp";
+import logo5 from "../assets/logos/miguelcamino.webp";
+import logo6 from "../assets/logos/porvenir.webp";
+import logo7 from "../assets/logos/centropedagogico.webp";
+import logo8 from "../assets/logos/italiaenlapatagonia.webp";
+import logo9 from "../assets/logos/primaria.webp";
+import logo10 from "../assets/logos/secundario.webp";
+import logo11 from "../assets/logos/iurp.webp";
+import logo12 from "../assets/logos/centrorecreativo.webp";
 
 // Array con logo y nombre
 const institutions = [
   { name: "Institución 1", logo: logo1 },
-  { name: "Institución 2", logo: logo1 },
-  { name: "Institución 3", logo: logo1 },
-  { name: "Institución 4", logo: logo1 },
-  { name: "Institución 5", logo: logo1 },
-  { name: "Institución 6", logo: logo1 },
-  { name: "Institución 7", logo: logo1 },
-  { name: "Institución 8", logo: logo1 },
-  { name: "Institución 9", logo: logo1 },
-  { name: "Institución 10", logo: logo1 },
-  { name: "Institución 11", logo: logo1 },
-  { name: "Institución 12", logo: logo1 },
+  { name: "Institución 2", logo: logo2 },
+  { name: "Institución 3", logo: logo3 },
+  { name: "Institución 4", logo: logo4 },
+  { name: "Institución 5", logo: logo5 },
+  { name: "Institución 6", logo: logo6 },
+  { name: "Institución 7", logo: logo7 },
+  { name: "Institución 8", logo: logo8 },
+  { name: "Institución 9", logo: logo9 },
+  { name: "Institución 10", logo: logo10 },
+  { name: "Institución 11", logo: logo11 },
+  { name: "Institución 12", logo: logo12 },
 ];
 
 export default function InstitucionesGrid() {
